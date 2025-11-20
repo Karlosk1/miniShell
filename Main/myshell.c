1389ab53eb5108f2892a50a4d1533bc81b12ef36
ItsJustPoul <pgbachs8987@gmail.com>
2025-11-20 12:55:12 +0100

    first update
    created main and two functions main depends on.

--- Main/myshell.c
+++ Main/myshell.c
@@ -1,5 +1,59 @@
-//
-// Created by Carlos Martín García on 5/11/25.
-//
-
 #include "myshell.h"
+#define MAXCOM 1000 // max number of letters to be supported
+#define MAXLIST 100 // max number of commands to be supported
+
+
+void init_shell() {
+    clear();
+    char* username = getenv("USER");
+    printf("\n\n\nUSER is: @%s\n", username);
+    sleep(1);
+    clear();
+}
+
+void printDir(){
+    char currentDirectory[1024];
+    getcwd(currentDirectory, sizeof(currentDirectory));
+    printf("\nDir: %s", currentDirectory);
+}
+
+int input(char* str){
+    char* buf;
+
+    //buf = readline("\n>>> ");
+    if (strlen(buf) != 0) {
+        add_history(buf);
+        strcpy(str, buf);
+        return 0;
+    } else {
+        return 1;
+    }
+}
+
+int main(){
+    char inputString[MAXCOM], *parsedArgs[MAXLIST];
+    char* parsedArgsPiped;
+    int flagger = 0; /*will flag zero if its void, 1 if its a simple command
+    and 2 if its including a pipe*/
+    init_shell();
+
+    while (1) {
+        // print shell line
+        printDir();
+        if (input(*inputString)==0){
+            continue;
+        }
+        
+        flagger = processString(inputString, parsedArgs, parsedArgsPiped);
+
+        // execute
+        if (flagger == 1){
+            execArgs(parsedArgs);
+        }
+
+        if (flagger == 2){
+            execArgsPiped(parsedArgs, parsedArgsPiped);
+        }
+    }
+    return 0;
+}
