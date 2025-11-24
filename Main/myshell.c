c7dc81199bbae5aa7d8c7a94f798663cdb0be490
Karlosk1 <carlos.martin.garcia.05@gmail.com>
2025-11-24 22:24:33 +0100

    Manejadores de funciones internas hecho. Funciones para manejo de jobs

--- Main/myshell.c
+++ Main/myshell.c
@@ -59,12 +59,23 @@ typedef struct { //Esto es basicamente un diccionario en c
     command_func func;  // puntero a la función que implementa el comando
 } command_entry;
 
+typedef struct job{
+    int id; //id del job
+    pid_t pgid; //id del grupo de proceso
+    char* comando; //comando que contiene
+    struct job* siguiente;
+}tNodeJob;
+
+typedef tNodeJob* tJobs;
+tJobs listaJobs = NULL;
+int siguienteId = 1;
 
 int manejador_cd(int argc, char *argv[]); //esto es la funcion que cambia de directorio pero cd no debne manejarse mediante proceso hijo porque el directorio es parte del contexto del proceso
 int manejador_exit(int argc, char *argv[]);
 int manejador_umask(int argc, char *argv[]);
 int manejador_jobs(int argc, char *argv[]); //Muestra la lista de jobs
 int manejador_fg(int argc, char *argv[]);
+tNodeJob* getJobXid(int id); //Obtiene el job mediante su id
 void add_job(pid_t pgid, const char *cmd);
 void remove_job(pid_t pgid);
 
@@ -78,16 +89,6 @@ command_entry diccionariodeComandos[] = {
 };
 
 
-typedef struct job{
-    int id; //id del job
-    pid_t pgid; //id del grupo de proceso
-    char* comando; //comando que contiene
-    struct job* siguiente;
-}tNodeJob;
-
-typedef tNodeJob* tJobs;
-tJobs listaJobs = NULL;
-int siguienteId = 1;
 
 
 int main(int argc, char* argv[]) {
@@ -167,12 +168,38 @@ int manejador_cd(int argc, char *argv[]) {
 }
 
 int manejador_exit(int argc, char *argv[]) {
-
+    printf("Saliendo de la miniShell \n");
+    exit(0);
 }
 
 int manejador_umask(int argc, char *argv[]) {
+    mode_t old_mask;
 
+    if (argc == 1) {
+        old_mask = umask(0);  // Obtener valor actual
+        umask(old_mask);      // Restaurar
+
+        printf("0%03o\n", old_mask);
+        return 0;
+    }
+
+    if (argc == 2) {
+        char *endptr;
+        mode_t new_mask = strtol(argv[1], &endptr, 8);
+
+        if (*endptr != '\0') {
+            fprintf(stderr, "umask: formato octal inválido: %s\n", argv[1]);
+            return 1;
+        }
+
+        umask(new_mask);
+        return 0;
+    }
+
+    fprintf(stderr, "Uso: umask [mode]\n");
+    return 1;
 }
+
 
 int manejador_jobs(int argc, char *argv[]){
     tNodeJob* actual = listaJobs;
@@ -182,6 +209,83 @@ int manejador_jobs(int argc, char *argv[]){
     }
 }
 
-int manejador_fg(int argc, char *argv[]);
+int manejador_fg(int argc, char *argv[]) {
+    int id;
 
+    if (argc > 1) {
+        id = atoi(argv[1]);
+    } else {
+        if (listaJobs == NULL) {
+            printf("fg: no hay trabajos\n");
+            return 1;
+        }
+        id = listaJobs->id; //coge el id del ultimo job
+    }
 
+    tNodeJob *cabeza = getJobXid(id);
+    if (cabeza == NULL) {
+        printf("fg: job %d no encontrado\n", id);
+        return 1;
+    }
+
+    // Dar el terminal al job
+    tcsetpgrp(STDIN_FILENO, cabeza->pgid); //Las señales que mandes al terminal las recibirá este job
+
+    printf("%s\n", cabeza->comando); //Imprime el commando que irá a fg
+
+    // Reanudar ejecución si está parado
+    kill(-cabeza->pgid, SIGCONT); //Envia la señal de reanudación a todo el grupo de procesos(-)
+
+    // Esperar hasta que termine
+    waitpid(-cabeza->pgid, NULL, WUNTRACED); //Espera el cambio(termine) en alguno del grupo. Cambiar el stado null a un int status y mirarlo
+
+    // Devolver terminal a la shell
+    tcsetpgrp(STDIN_FILENO, getpgrp()); //Devuelve el control del terminal a shell y devuelve el pgid del proceso actual, que es la shell
+
+    remove_job(cabeza->pgid); //Elimina el job de la lista interna. Ya ha terminado. Hay que comprobar que todos los procesos del grupo han terminado
+    return 0;
+}
+
+tNodeJob* getJobXid(int id) {
+    tNodeJob* nodo = listaJobs;
+    while (nodo!= NULL) {
+        if (nodo->id == id) {
+            return nodo;
+        }
+        nodo = nodo->siguiente;
+    }
+    return NULL;
+}
+
+void add_job(pid_t pgid, const char *cmd) {
+    tNodeJob* nodo = (tNodeJob*) malloc(sizeof(tNodeJob));
+    nodo->pgid =pgid;
+    strcpy(nodo->comando, cmd);
+    nodo->siguiente = listaJobs;
+    listaJobs = nodo;
+}
+
+void remove_job(pid_t pgid) {
+    // Búsqueda
+    int encontrado = 0;
+    tNodeJob * pAnt = NULL;
+    tNodeJob * pAct = listaJobs;
+    while (!encontrado && pAct != NULL) {
+        if (pAct->pgid == pgid) {
+            encontrado = 1;
+        } else {
+            pAnt = pAct;
+            pAct = pAct->siguiente;
+        }
+    }
+
+    // Borrado
+    if (encontrado) {
+        if (pAnt == NULL) { // if (pAct == *l) { Eliminar 1º
+            listaJobs = (listaJobs)->siguiente;
+        } else { // Eliminar cualquiera salvo el 1º
+            pAnt->siguiente = pAct->siguiente;
+        }
+        free(pAct);
+    }
+}
